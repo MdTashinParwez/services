5c18e959ce30518fcf91f21fbfd188cab530add8
MdTashinParwez <tashinparwez537@gmail.com>
2026-07-07 14:20:31 +0530

    feat: enhance UpdateService function with improved error handling and image upload support

--- src/controllers/service.controller.js
+++ src/controllers/service.controller.js
@@ -133,18 +133,21 @@ const UpdateService = asyncHandler(async (req,res) => {
   const {id} = req.params;
 
   if(!mongoose.isValidObjectId(id)){
-    throw new apiError(402,"Invalid Id")
+    throw new apiError(400,"Invalid Id")
   }
 
 const service = await Service.findById(id); // yaha se id, provider, sab k access miljayga  us servic database m id se 
 
  if(!service){
-      throw new apiError(402,"No Service found")
+      throw new apiError(404,"No Service found")
  }
 
  const currentProvider = await Provider.findOne({
   user: req.user._id,
  })
+ if(!currentProvider){
+  throw new apiError(404, "provide not found");
+ }
  if (service.provider.toString() != currentProvider._id.toString()){
     throw new apiError(403, "You are not allowed to update this service");
 
@@ -203,11 +206,90 @@ const service = await Service.findById(id); // yaha se id, provider, sab k acces
     );
   }
 
-  
+  // IMAGES
+  let imageUrls = service.images;
 
+  const imageFiles = req.files?.images || [];
+
+  if (imageFiles.length > 0) {
+    imageUrls = [];
+
+    for (const file of imageFiles) {
+      const uploadedImage = await uploadOnCloudinary(file.path);
+
+      if (!uploadedImage?.url) {
+        throw new apiError(500, "Image upload failed");
+      }
+
+      imageUrls.push(uploadedImage.url);
+    }
+  }
+
+  // update 
+  const updateData = {};
+
+  if(title !== undefined){
+    updateData.title = title.trim();
+  }
+  if (description !== undefined) {
+    updateData.description = description.trim();
+  }
+
+  if (category !== undefined) {
+    updateData.category = category;
+  }
+
+  if (price !== undefined) {
+    updateData.price = Number(price);
+  }
+
+  if (duration !== undefined) {
+    updateData.duration = Number(duration);
+  }
+
+  if (location !== undefined) {
+    updateData.location = location;
+  }
+
+  if (serviceType !== undefined) {
+    updateData.serviceType = serviceType;
+  }
+
+  if (tags !== undefined) {
+    updateData.tags = Array.isArray(tags)
+      ? tags.map(tag => tag.trim().toLowerCase())
+      : [];
+  }
+
+  if (customFields !== undefined) {
+    updateData.customFields = customFields;
+  }
+
+  updateData.images = imageUrls;
+
+  const updateService = await Service.findByIdAndUpdate(
+    id,updateData,
+    {
+      new: true, 
+      runValidators: true,
+
+    }
+  )
+    .populate("provider", "businessName isVerified")
+    .populate("category", "name slug");
+
+ return res.status(200).json(
+  new ApiResponse(
+    200,
+    updateService,
+    "Service updated successfully"
+  )
+);
 
 
 })
+
+
 
 export{
   createService
