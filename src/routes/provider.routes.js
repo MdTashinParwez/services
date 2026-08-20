ccdcbee6f020d4918d1958a925651b11c48a40a2
MdTashinParwez <tashinparwez537@gmail.com>
2026-08-20 23:23:37 +0530

    feat: reorder provider routes to place getProviderById after getProviderStatus

--- src/routes/provider.routes.js
+++ src/routes/provider.routes.js
@@ -12,6 +12,6 @@ router.route("/profile").patch(verifyJWT,updateProviderDetail)
 router.route("/documents").patch(verifyJWT,upload.single("documents"),updateProviderDocument)
 router.route("/me").get(verifyJWT,getcurrentProvider)
 router.route("/all").get(verifyJWT,getAllProviders)
-router.route("/:id").get(verifyJWT,getProviderById)
 router.get("/status", verifyJWT, getProviderStatus);
+router.route("/:id").get(verifyJWT,getProviderById)
 export default router
