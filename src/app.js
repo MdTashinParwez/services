db683709951b532db81de073e0fbb1e37387fce1
MdTashinParwez <tashinparwez537@gmail.com>
2026-09-07 23:37:22 +0530

    feat: add socket.io dependency and update package.json and package-lock.json

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ origin: process.env.CORS_ORIGIN,
 credentials: true
 }));
 
+
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
@@ -54,7 +55,6 @@ app.use("/health", healthRouter); // redis health
 // error handling 
 app.use(errorHandler);
 
+
+
 export{app} 
-
-
-// check git restoring
