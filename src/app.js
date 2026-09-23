c4ac923e1db83b27522ed0ca27a10ed2c8ca93f1
MdTashinParwez <tashinparwez537@gmail.com>
2026-09-23 21:58:55 +0530

    feat: add webhook endpoint for Razorpay payment notifications

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/error.middleware.js";
+import { razorpayWebhook } from "./controllers/payment.controller.js";
 
 const app = express();
 app.use(cors({
@@ -9,6 +10,12 @@ origin: process.env.CORS_ORIGIN,
 credentials: true
 }));
 
+
+app.post(
+  "/api/v1/payment/webhook",
+  express.raw({ type: "application/json" }),
+  razorpayWebhook
+);
 
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
